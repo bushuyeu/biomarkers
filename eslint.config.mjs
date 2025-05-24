3b495950682423912e7130d61ceafbe5b2f4dccb
Pavel Bushuyeu <pavelbushuyeu@gmail.com>
2025-05-24 10:08:11 -0700

    ESLint is now configured to ignore unused variables if their names start with an underscore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }  // Allow unused variables prefixed with "_"
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
